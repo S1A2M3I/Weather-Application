062273069b83190d03cd59c660e561d329499b61
S1A2M3I <108212900+S1A2M3I@users.noreply.github.com>
2024-12-15 10:34:20 +0530

    first commit

---
+++ api.js
@@ -0,0 +1,49 @@
+//Async JS
+
+// console.log("Navbar");
+// setTimeout(( ) => {
+//     console.log("Navbar Data");
+// },3000)
+
+// console.log("Body");
+// setTimeout(( ) => {
+//     console.log("Body Data");
+// },2000)
+
+// console.log("Footer");    
+
+// setInterval(() => {
+//     console.log("Footer Data");
+// },1000)
+
+// setInterval(() => {
+//     console.log("hello");
+// },2000)
+
+
+
+
+//API   APPLICATION PROGRAMMING INTERFACE
+// const url =" https://cat-fact.herokuapp.com/facts"
+// let data = fetch("https://cat-fact.herokuapp.com/facts")
+
+
+
+// let para = document.getElementById("parafacts");
+// let btn = document.getElementById("btn");
+
+// console.log("body");
+// async function facts(){
+//     console.log("Getting Data");
+//     let data = await fetch(url)
+//     console.log(data);
+//     let actualData = await data.json()
+//     for (let i=0;i<actualData.length;i++){
+//     let info = document.createElement("p");
+//     info.innerText = actualData[i].text;
+//     para.appendChild(info);
+//     }
+// }
+// console.log("footer");
+
+// btn.addEventListener("click", facts);
